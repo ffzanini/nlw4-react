c6f3424f12ccd8a3eb3ce0c3200b59d9684e1f73
Felipe Frantz Zanini <devffzanini@gmail.com>
2021-02-24 10:51:57 -0300

    day two

---
+++ moveit-next/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+//aqui ficam todas as funcionalidades que irão se repetir no desenvolvimento e é recalculado para o novo processamento
+
+import '../styles/global.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
